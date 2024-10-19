0f9d0322c73884152a8b5975178f2d32d0cab163
unknown <harshavardhanputta5@gmail.com>
2024-10-19 10:55:37 +0530

    Update social links ordering

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -4,7 +4,7 @@ import SocialLinks from "./SocialLinks";
 const Connect = () => {
   return (
     <div className="flex justify-center h-5/6 uppercase">
-      <div className="flex justify-around w-full h-full">
+      <div className="flex justify-start w-full h-full">
         <div className="flex align-bottom sm:w-1/2 w-2/6 h-full bg-white sm:text-5xl text-3xl font-extrabold py-4 sm:px-4 tracking-wider">
           <SocialLinks />
         </div>
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -6,17 +6,17 @@ const SocialLinks = () => {
       <a
         target="_blank"
         rel="noopener noreferrer"
-        href="mailto:harshavardhanputta5@gmail.com"
+        href="https://www.instagram.com/harshavardhan.putta/"
         className="w-1/4 h-fit sm:text-start text-end text-red-200 hover:text-black"
         style={{ writingMode: "vertical-lr" }}
       >
-        Gmail
+        Instagram
       </a>
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://www.linkedin.com/in/harshavardhan-putta-53b510238/"
-        className="w-1/4 h-fit sm:text-start text-end text-red-300 hover:text-black"
+        className="w-1/4 h-fit sm:text-start text-end text-red-400 hover:text-black"
         style={{ writingMode: "vertical-lr" }}
       >
         LinkedIn
@@ -24,11 +24,20 @@ const SocialLinks = () => {
       <a
         target="_blank"
         rel="noopener noreferrer"
-        href="https://www.instagram.com/harshavardhan.putta/"
-        className="w-1/4 h-fit sm:text-start text-end text-red-400 hover:text-black"
+        href="https://github.com/har5hcodes"
+        className="w-1/4 h-fit sm:text-start text-end text-red-500 hover:text-black"
         style={{ writingMode: "vertical-lr" }}
       >
-        Instagram
+        Github
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="mailto:harshavardhanputta5@gmail.com"
+        className="w-1/4 h-fit sm:text-start text-end text-red-700 hover:text-black"
+        style={{ writingMode: "vertical-lr" }}
+      >
+        Gmail
       </a>
     </>
   );
